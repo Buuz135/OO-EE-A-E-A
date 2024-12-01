cc8ae6c2f39e908e86b836c47785b410bed5eb65
Buuz135 <buuz135@hotmail.com>
2024-12-01 14:20:39 +0100

    Inital Commit

--- src/main/java/com/buuz135/oo_ee_a_e_a/Oo_ee_a_e_a.java
+++ src/main/java/com/buuz135/oo_ee_a_e_a/Oo_ee_a_e_a.java
@@ -2,109 +2,57 @@ package com.buuz135.oo_ee_a_e_a;
 
 import com.mojang.logging.LogUtils;
 import net.minecraft.client.Minecraft;
-import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
-import net.minecraft.network.chat.Component;
-import net.minecraft.world.food.FoodProperties;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.MapColor;
-import net.neoforged.api.distmarker.Dist;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.entity.animal.Cat;
+import net.minecraft.world.phys.AABB;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.ModContainer;
-import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
-import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
-import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.client.event.ClientTickEvent;
 import net.neoforged.neoforge.common.NeoForge;
-import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
-import net.neoforged.neoforge.event.server.ServerStartingEvent;
-import net.neoforged.neoforge.registries.DeferredBlock;
 import net.neoforged.neoforge.registries.DeferredHolder;
-import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 import org.slf4j.Logger;
 
 
 @Mod(Oo_ee_a_e_a.MODID)
 public class Oo_ee_a_e_a {
-    // Define mod id in a common place for everything to reference
+
     public static final String MODID = "oo_ee_a_e_a";
-    // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 
+    public static DeferredRegister<SoundEvent> SOUNDS = DeferredRegister.create(Registries.SOUND_EVENT, MODID);
+    public static DeferredHolder<SoundEvent, SoundEvent> FAST = SOUNDS.register("oo_ee_a_e_a_fast", (rl) -> SoundEvent.createFixedRangeEvent(rl, 15));
+    public static DeferredHolder<SoundEvent, SoundEvent> SLOW = SOUNDS.register("oo_ee_a_e_a_slow", (rl) -> SoundEvent.createFixedRangeEvent(rl, 15));
 
-    // Creates a new food item with the id "oo_ee_a_e_a:example_id", nutrition 1 and saturation 2
-    public static final DeferredItem<Item> EXAMPLE_ITEM = ITEMS.registerSimpleItem("example_item", new Item.Properties().food(new FoodProperties.Builder().alwaysEdible().nutrition(1).saturationModifier(2f).build()));
 
-    // Creates a creative tab with the id "oo_ee_a_e_a:example_tab" for the example item, that is placed after the combat tab
-    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> EXAMPLE_TAB = CREATIVE_MODE_TABS.register("example_tab", () -> CreativeModeTab.builder().title(Component.translatable("itemGroup.oo_ee_a_e_a")).withTabsBefore(CreativeModeTabs.COMBAT).icon(() -> EXAMPLE_ITEM.get().getDefaultInstance()).displayItems((parameters, output) -> {
-        output.accept(EXAMPLE_ITEM.get()); // Add the example item to the tab. For your own tabs, this method is preferred over the event
-    }).build());
-
-    // The constructor for the mod class is the first code that is run when your mod is loaded.
-    // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
     public Oo_ee_a_e_a(IEventBus modEventBus, ModContainer modContainer) {
-        // Register the commonSetup method for modloading
-        modEventBus.addListener(this::commonSetup);
+        SOUNDS.register(modEventBus);
 
-        // Register the Deferred Register to the mod event bus so blocks get registered
-        BLOCKS.register(modEventBus);
-        // Register the Deferred Register to the mod event bus so items get registered
-        ITEMS.register(modEventBus);
-        // Register the Deferred Register to the mod event bus so tabs get registered
-        CREATIVE_MODE_TABS.register(modEventBus);
-
-        // Register ourselves for server and other game events we are interested in.
-        // Note that this is necessary if and only if we want *this* class (Oo_ee_a_e_a) to respond directly to events.
-        // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
+        modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
         NeoForge.EVENT_BUS.register(this);
 
-        // Register the item to a creative tab
-        modEventBus.addListener(this::addCreative);
-
-        // Register our mod's ModConfigSpec so that FML can create and load the config file for us
-        modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
 
-    private void commonSetup(final FMLCommonSetupEvent event) {
-        // Some common setup code
-        LOGGER.info("HELLO FROM COMMON SETUP");
+    public static long lastSpin = -1;
+    public static boolean fast = true;
 
-        if (Config.logDirtBlock) LOGGER.info("DIRT BLOCK >> {}", BuiltInRegistries.BLOCK.getKey(Blocks.DIRT));
-
-        LOGGER.info(Config.magicNumberIntroduction + Config.magicNumber);
-
-        Config.items.forEach((item) -> LOGGER.info("ITEM >> {}", item.toString()));
-    }
-
-    // Add the example block item to the building blocks tab
-    private void addCreative(BuildCreativeModeTabContentsEvent event) {
-        if (event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS) event.accept(EXAMPLE_BLOCK_ITEM);
-    }
-
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
-    public void onServerStarting(ServerStartingEvent event) {
-        // Do something when the server starts
-        LOGGER.info("HELLO from server starting");
-    }
-
-    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-    @EventBusSubscriber(modid = MODID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-    public static class ClientModEvents {
-        @SubscribeEvent
-        public static void onClientSetup(FMLClientSetupEvent event) {
-            // Some client setup code
-            LOGGER.info("HELLO FROM CLIENT SETUP");
-            LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
+    public void clientTickEvent(ClientTickEvent.Pre event) {
+        var level = Minecraft.getInstance().level;
+        if (Minecraft.getInstance().isPaused() || level == null) return;
+        if (lastSpin == -1) {
+            lastSpin = level.getGameTime();
+        }
+        if (level.getGameTime() > lastSpin + Config.COOLDOWN_BETWEEN_SPINS.getAsInt()*20 && level.random.nextFloat() < 0.05) {
+            lastSpin = level.getGameTime();
+            fast = level.random.nextBoolean();
+            var entities = level.getEntitiesOfClass(Cat.class, new AABB(Minecraft.getInstance().player.blockPosition()).inflate(32));
+            entities.forEach(cat -> level.playLocalSound(cat, fast ? FAST.get() : SLOW.get(), SoundSource.NEUTRAL, 1,1));
         }
     }
 }
--- src/main/java/com/buuz135/oo_ee_a_e_a/mixin/CatRendererMixin.java
+++ src/main/java/com/buuz135/oo_ee_a_e_a/mixin/CatRendererMixin.java
@@ -1,4 +1,27 @@
 package com.buuz135.oo_ee_a_e_a.mixin;
 
+import com.buuz135.oo_ee_a_e_a.Oo_ee_a_e_a;
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.math.Axis;
+import net.minecraft.client.renderer.entity.CatRenderer;
+import net.minecraft.world.entity.animal.Cat;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(CatRenderer.class)
 public class CatRendererMixin {
+
+    @Inject(method = "setupRotations(Lnet/minecraft/world/entity/animal/Cat;Lcom/mojang/blaze3d/vertex/PoseStack;FFFF)V", at = @At("HEAD"), cancellable = false)
+    public void setupRotationMixin(Cat entity, PoseStack poseStack, float bob, float yBodyRot, float partialTick, float scale, CallbackInfo ci){
+        rotateCat(entity, poseStack, bob, yBodyRot, partialTick, scale);
+    }
+
+
+    public void rotateCat(Cat entity, PoseStack poseStack, float bob, float yBodyRot, float partialTick, float scale){
+        if (entity.level().getGameTime() - Oo_ee_a_e_a.lastSpin < 2*20){
+            poseStack.mulPose(Axis.YP.rotation(entity.level().getGameTime() % (Oo_ee_a_e_a.fast ? 90 : 180) + partialTick));
+        }
+    }
 }
